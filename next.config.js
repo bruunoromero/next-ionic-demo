0f4cd5e2089b3f2e87e9cc2de43863b4bcd01001
christian <christian.a.baird@gmail.com>
2022-01-19 22:00:44 -0500

    Trying fixes for ionic and next.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+const withTM = require('next-transpile-modules')(['@ionic/react', '@ionic/core', '@stencil/core', 'ionicons']);
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withTM({
   reactStrictMode: true,
-}
+});
